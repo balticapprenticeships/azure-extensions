c2c12e64dedba378940a47c4d842036534afcdeb
Chris Langford <clangford@lang-ford.uk>
2019-11-07 15:45:02 +0000

    Added GB regional settings files

---
+++ regional-settings/regionalSettings.ps1
@@ -0,0 +1,29 @@
+######################################
+# Script to define regional settings on Azure Virtual Machines
+# Author: Chris Langford
+######################################
+
+#variables
+$regionalsettingsURL = "https://raw.githubusercontent.com/balticapprenticeships/azure-extensions/master/gbRegional.xml"
+$RegionalSettings = "D:\gbRegion.xml"
+
+
+#downdload regional settings file
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+
+
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+
+# Set languages/culture. Not needed perse.
+Set-WinSystemLocale en-GB
+Set-WinUserLanguageList -LanguageList en-GB -Force
+Set-WinUILanguageOverride -Language en-GB
+Set-Culture -CultureInfo en-GB
+Set-WinHomeLocation -GeoId 242
+Set-TimeZone -Name "GMT Standard Time"
+
+# restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
+Start-sleep -Seconds 30
+Restart-Computer
