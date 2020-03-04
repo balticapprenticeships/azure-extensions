89f6e32fa6d9d5d2eee9f291bce3f3c8231c71fd
Chris Langford <clangford@lang-ford.uk>
2020-03-04 17:45:56 +0000

    Updated xUser Dsc

--- DSC/xCreateUserDsc.ps1
+++ DSC/xCreateUserDsc.ps1
@@ -2,11 +2,7 @@ Configuration xUser_CreateUserConfig {
     [CmdletBinding()]
 
     Param (
-
-        [Parameter(Mandatory = $true)]
-        [string]
-        $nodeName,
-
+        
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
